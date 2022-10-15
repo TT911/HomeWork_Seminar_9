970cad18b3d3b5f62b90055d43f3dd71a85a2395
BSerge <serge.boskin@icloud.com>
2022-10-15 16:01:47 +0300

    add Objective_66

---
+++ Objective_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Sum(int m, int n)
+{
+    if(n == m)
+    {
+        return m;
+    }
+    return n + Sum(m, n-1);
+}
+Console.WriteLine(Sum(m,n));
