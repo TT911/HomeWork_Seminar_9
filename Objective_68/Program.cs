29e36afa183a3cb094ed0e4ea1c6f13c1c400983
BSerge <serge.boskin@icloud.com>
2022-10-15 16:08:45 +0300

    add Objective_68

---
+++ Objective_68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Akker(int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    if(m > 0 && n == 0)
+    {
+        return Akker(m - 1, 1);
+    }
+    return Akker(m - 1, Akker(m, n -1));
+}
+Console.WriteLine(Akker(m,n));
