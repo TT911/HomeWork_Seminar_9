a584a73e69752d75469a64dcc0e00f3ee90a186c
BSerge <serge.boskin@icloud.com>
2022-10-15 15:41:03 +0300

    add Objective_64

---
+++ Objective_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+
+int n = 1;
+
+string Recurs (int m, int n)
+{
+    if(m == n)
+    {
+        return n.ToString();
+    }
+    return (m + " " + Recurs(m - 1, n));
+
+}
+
+Console.WriteLine(Recurs(m, n));
